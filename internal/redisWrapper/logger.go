e4b703af5adef2657befe8932c93d39febebe7c9
Luke Kim <burgund32@gmail.com>
2020-02-14 06:59:17 +0900

    Data Log file을 읽어 parameter로 전달받은 hash map에 저장하는 함수

--- internal/redisWrapper/logger.go
+++ internal/redisWrapper/logger.go
@@ -1,18 +1,34 @@
 package redisWrapper
 
 import (
+	"bufio"
 	"fmt"
 	"interface_hash_server/internal/hash"
+	"interface_hash_server/internal/models"
+	"interface_hash_server/tools"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // dataLoggers gets a logger by passed-key of Each Node address
 var dataLoggers map[string] /* key = each Node's address*/ *log.Logger
 
+type logFormat struct {
+	models.KeyValuePair
+	Command string
+}
+
 const (
 	//LogDirectory is a directory path where log files are saved
 	logDirectory = "./internal/redisWrapper/dump"
+
+	/* constants for "index" of Data Log each lines */
+	hashIndexWord = 0
+	commandWord   = 1
+	keyWord       = 2
+	valueWord     = 3
 )
 
 // SetUpModificationLogger 는 Data Modification이 일어날 때 파일에 기록을 하기 위한 로거 세터
@@ -44,12 +60,49 @@ func SetUpModificationLogger(nodeAddressList []string) {
 func RecordModification(address string, command string, key string, value string) error {
 
 	targetDataLogger, isSet := dataLoggers[address]
-	if isSet != false {
+	if isSet == false {
 		return fmt.Errorf("RecordModification() : data Logger is not set up")
 	}
 
 	hashSlotIndex := hash.GetHashSlotIndex(key)
 	targetDataLogger.Printf("%d %s %s %s", hashSlotIndex, command, key, value)
+
+	return nil
+}
+
+// readDataLogs reads Node's data log file and records the information in @hashIndexToKeyValuePairMap
+func readDataLogs(address string, hashIndexToLogFormatMap map[uint16][]logFormat) error {
+	filePath := fmt.Sprintf("%s/%s", logDirectory, address)
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("readDataLogs() : opening file %s error - %s", filePath, err.Error())
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words := strings.Fields(scanner.Text())
+		hashIndexIn64, err := strconv.ParseUint(words[hashIndexWord], 10, 16)
+		if err != nil {
+			return fmt.Errorf("readDataLogs() : Parsing hash index error")
+		}
+
+		hashIndex := uint16(hashIndexIn64)
+
+		var logFormat logFormat
+		logFormat.Key = words[keyWord]
+		logFormat.Value = words[valueWord]
+		logFormat.Command = words[commandWord]
+
+		tools.InfoLogger.Printf("readDataLogs() : data log file read result : %d %s %s %s\n",
+			hashIndex, words[commandWord], words[keyWord], words[valueWord])
+
+		hashIndexToLogFormatMap[hashIndex] = append(hashIndexToLogFormatMap[hashIndex], logFormat)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("readDataLogs() : scanner error - %s", err.Error())
+	}
 
 	return nil
 }
